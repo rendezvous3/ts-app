1de597319496a675a753b6c20e03f44963c510f5
Bojan Jovanovic <bojanj6m@gmail.com>
2020-05-13 05:02:30 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,20 @@
 import React, { useContext } from 'react';
-import { Store, actionsTypes } from './Store';
-import { IAction, IEpisode, IEpisodeProps } from './interfaces';
+import { Store } from './Store';
+import { Link } from '@reach/router';
 
-const EpisodeList = React.lazy<any>(() => import('./EpisodesList'));
-
-export default function App(): JSX.Element {
-  const { state, dispatch } = useContext(Store);
-
-  React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction();
-  });
-
-  const fetchDataAction = async () => {
-    const URL =
-      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
-    const data = await fetch(URL);
-    const dataJSON = await data.json();
-    return dispatch({
-      type: actionsTypes.FETCH_DATA,
-      payload: dataJSON._embedded.episodes,
-    });
-  };
-
-  const toggleFavAction = (episode: IEpisode): IAction => {
-    const episodeInFav = state.favorites.includes(episode);
-    let dispatchObj = {
-      type: actionsTypes.ADD_FAVORITES,
-      payload: episode,
-    };
-    if (episodeInFav) {
-      const favWithoutEpisode = state.favorites.filter(
-        (fav: IEpisode) => fav.id !== episode.id
-      );
-      dispatchObj = {
-        type: actionsTypes.REMOVE_FAVORITES,
-        payload: favWithoutEpisode,
-      };
-    }
-    return dispatch(dispatchObj);
-  };
-
-  const episodeListProps: IEpisodeProps = {
-    episodes: state.episodes,
-    favorites: state.favorites,
-    toggleFavAction: toggleFavAction
-  };
-
-  console.log('STATE ', state);
+export default function App(props: any): JSX.Element {
+  const { state } = useContext(Store);
   return (
     <div data-testid='component-app'>
-      <h1>Rick and Morty</h1>
-      <p>Pick your favorite episode</p>
-      <div data-testid='episodes-list'>
-        {state.episodes.length && (
-          <React.Suspense fallback={<div>...Loading</div>}>
-            <EpisodeList {...episodeListProps} />
-          </React.Suspense>
-        )}
+      <div>
+        <h1>Rick and Morty</h1>
+        <p>Pick your favorite episode</p>
       </div>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/favorites'>Favorite(s): {state.favorites.lengh}</Link>
+      </div>
+      {props.children}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { StoreProvider } from './Store';
+import { Router, RouteComponentProps } from '@reach/router';
+import HomePage from './HomePage';
+import FavoritesPage from './FavoritesPage';
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
 
 ReactDOM.render(
   <StoreProvider>
-    <App />
+    <Router>
+      <App path='/'>
+        <RouterPage pageComponent={<HomePage/>} path='/' />
+        <RouterPage pageComponent={<FavoritesPage/>} path='/favorites' />
+      </App>
+    </Router>
   </StoreProvider>,
   document.getElementById('root')
 );
